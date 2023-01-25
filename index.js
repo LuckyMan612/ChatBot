61bb0bba16d4c97c970f8271c74c8d031f2cf095
LuckyMan612 <75744132+LuckyMan612@users.noreply.github.com>
2023-01-25 19:02:39 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+function sendMessage() {
+  // Pobieranie wiadomości z inputu
+  var message = document.getElementById("message").value;
+
+  // Wysyłanie wiadomości do API
+  fetch("https://api.popcat.xyz/chatbot?msg=" + message)
+    .then(function(response) {
+      // Sprawdzanie czy API jest online
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("API jest offline.");
+      }
+    })
+    .then(function(data) {
+      // Wyświetlanie odpowiedzi
+      var history = document.getElementById("history");
+      var newMessage = document.createElement("div");
+      newMessage.innerHTML = "<b>Ty:</b> " + message;
+      history.appendChild(newMessage);
+      newMessage = document.createElement("div");
+      newMessage.innerHTML = "<b>ChatBot:</b> " + data.response.response;
+      history.appendChild(newMessage);
+      // Czyszczenie inputu
+      document.getElementById("message").value = "";
+    })
+    .catch(function(error) {
+      // Wyświetlanie błędu
+      alert(error);
+    });
+}
